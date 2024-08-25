e8083ac008dade44873a553733748d94c3338f8f
Irfan Budiono <irfanbudiono697@gmail.com>
2024-08-25 14:02:05 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Git remote</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, ipsam cum itaque saepe ratione ullam fugiat adipisci consequatur neque, sint voluptate minima beatae odio quis consequuntur in eum. Ea, eaque.</p>
 </body>
 </html>
